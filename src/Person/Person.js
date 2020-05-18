f4bdb0f0a13a9d702e5834066935042331005f8f
jenibiliyawala <jenibiliyawala>
2020-05-18 18:21:15 +0530

    First Commit

---
+++ src/Person/Person.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const person = (props) => {
+    return (
+        <div>
+            <p>I'm {props.name} and I am {props.age} years old..!</p>
+        </div>
+    )
+};
+
+export default person;
